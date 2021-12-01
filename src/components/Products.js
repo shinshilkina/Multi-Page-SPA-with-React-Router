608e0d445a9cccc33be7a03b819ba2300c10fd7b
shinshilkina <anastasiashilina98@gmail.com>
2021-12-01 19:19:18 +0700

    Defining & Using Routes

---
+++ src/components/Products.js
@@ -0,0 +1,5 @@
+const Products = (props) => {
+  return <h1>The Products Page</h1>;
+};
+
+export default Products;
---
+++ src/components/Welcome.js
@@ -0,0 +1,5 @@
+const Welcome = (props) => {
+  return <h1>The Welcome Page</h1>;
+};
+
+export default Welcome;
